6e9d159c9589d818cbc1055201b9fb83586fff5f
TheodoreDev <theodore.delbove@gmail.com>
2023-04-28 12:18:43 +0200

    Add the map of the city Add teleport point between maps

---
+++ map/Jeu de tuiles (.tsx)/terrain_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="terrain_3" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../../image/map/terrain_3.png" trans="000000" width="1024" height="1024"/>
+</tileset>

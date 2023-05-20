08f1237c65001a21be67f7a8b734cc1e11cf6946
Théodore <theodore.delbove@gmail.fr>
2023-05-20 18:45:16 +0200

    Add inventory Add 2 out of 3 objects Add the parking

---
+++ map/Jeu de tuiles (.tsx)/camaro6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="camaro6" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="../../image/player-PNJ/camaro6.png" trans="000000" width="32" height="32"/>
+</tileset>
---
+++ map/Jeu de tuiles (.tsx)/parking_ground_terminus2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="parking_ground_terminus2" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="../../image/map/parking_ground_terminus2.png" trans="003039" width="32" height="32"/>
+</tileset>
